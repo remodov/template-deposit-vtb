4604439c32eb6b999feef2d513ed372ef7bd9746
vadimvikulin <vvikulin@belkacar.ru>
2024-05-06 09:31:01 +0300

    add openapi generator

---
+++ src/main/java/com/example/deposit/api/PetApiController.java
@@ -0,0 +1,8 @@
+package com.example.deposit.api;
+
+import com.example.deposit.rest.api.PetApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PetApiController implements PetApi {
+}
---
+++ src/main/java/com/example/deposit/api/StoreApiController.java
@@ -0,0 +1,8 @@
+package com.example.deposit.api;
+
+import com.example.deposit.rest.api.StoreApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StoreApiController implements StoreApi {
+}
